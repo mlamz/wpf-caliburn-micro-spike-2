e2149aa68163e43613984373fc0d452ff19d4c78
Michael Lam <michael@mapletonhillmedia.com>
2012-12-19 09:18:43 -0700

    use of caliburn data binding

--- CaliburnMicroSpike2/CaliburnMicroSpike2/AppView.xaml.cs
+++ CaliburnMicroSpike2/CaliburnMicroSpike2/AppView.xaml.cs
@@ -13,38 +13,5 @@ namespace CaliburnMicroSpike2
 		{
 			InitializeComponent();
 		}
-
-		private void textBox1_MouseDoubleClick(object sender, MouseButtonEventArgs e)
-		{
-			var textBox = sender as TextBox;
-			SetTextBoxEdit(textBox);
-		}
-
-		private void textBox1_KeyDown(object sender, KeyEventArgs e)
-		{
-			var key = e.Key;
-			if (key == Key.Return)
-			{
-				SetTextBoxReadonly((TextBox)sender);
-			}
-		}
-
-		private void SetTextBoxReadonly(TextBox textBox)
-		{
-			textBox.IsReadOnly = true;
-			var backgroundColor = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#333333"));
-			textBox.Background = backgroundColor;
-			textBox.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#AAAAAA"));
-			textBox.CaretBrush = backgroundColor;
-		}
-
-		private void SetTextBoxEdit(TextBox textBox)
-		{
-			textBox.IsReadOnly = false;
-			textBox.Background = new SolidColorBrush(Colors.DarkSlateGray);
-			textBox.Foreground = new SolidColorBrush(Colors.White);
-			textBox.CaretBrush = new SolidColorBrush(Colors.White);
-			textBox.Focus();
-		}
 	}
 }
--- CaliburnMicroSpike2/CaliburnMicroSpike2/AppViewModel.cs
+++ CaliburnMicroSpike2/CaliburnMicroSpike2/AppViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using System.ComponentModel.Composition;
+using System.Windows;
+using System.Windows.Input;
 using Caliburn.Micro;
 
 namespace CaliburnMicroSpike2
@@ -6,6 +8,54 @@ namespace CaliburnMicroSpike2
 	[Export(typeof(AppViewModel))]
 	public class AppViewModel: PropertyChangedBase
 	{
+		private string _textBox1 = "item 1";
+		private bool _textBoxReadOnly = true;
+		private string _textBox1Background = "#666666";
+
+		public string TextBox1
+		{
+			get { return _textBox1; }
+			set
+			{
+				_textBox1 = value;
+				NotifyOfPropertyChange(() => TextBox1);
+			}
+		}
 		
+		public void OpenItem(object sender, object eventArgs)
+		{
+			TextBox1ReadOnly = false;
+			TextBox1Background = "#999999";
+			TextBox1 += ".";
+		}
+
+		public bool TextBox1ReadOnly
+		{
+			get { return _textBoxReadOnly; }
+			set
+			{
+				_textBoxReadOnly = value;
+				NotifyOfPropertyChange(() => TextBox1ReadOnly);
+			}
+		}
+
+		public string TextBox1Background
+		{
+			get { return _textBox1Background; }
+			set
+			{
+				_textBox1Background = value;
+				NotifyOfPropertyChange(() => TextBox1Background);
+			}
+		}
+
+		public void KeyDown(KeyEventArgs eventArgs)
+		{
+			if (eventArgs.Key == Key.Return)
+			{
+				TextBox1ReadOnly = true;
+				TextBox1Background = "#666666";
+			}
+		}
 	}
 }
